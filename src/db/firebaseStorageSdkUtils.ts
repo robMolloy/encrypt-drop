db085ec6e65d2dab312d2dd410c14cc4ae3db214
robmolloy <robmolloy@hotmail.co.uk>
2025-01-19 21:00:40 +0000

    getFiles downloading correctly

--- src/db/firebaseStorageSdkUtils.ts
+++ src/db/firebaseStorageSdkUtils.ts
@@ -1,10 +1,22 @@
-import { FirebaseStorage, ref, uploadBytes } from "firebase/storage";
+import { FirebaseStorage, ref, uploadBytes, getBlob } from "firebase/storage";
 
 export const uploadFileBlob = async (p: { storage: FirebaseStorage; id: string; blob: Blob }) => {
   try {
     const storageRef = ref(p.storage, `files/${p.id}`);
     const snapshot = await uploadBytes(storageRef, p.blob);
     return { success: true, data: snapshot } as const;
+  } catch (e) {
+    const error = e as { message: string };
+    console.error(error);
+    return { success: false, error } as const;
+  }
+};
+
+export const downloadFileBlob = async (p: { storage: FirebaseStorage; id: string }) => {
+  try {
+    const storageRef = ref(p.storage, `files/${p.id}`);
+    const blob = await getBlob(storageRef);
+    return { success: true, data: blob } as const;
   } catch (e) {
     const error = e as { message: string };
     console.error(error);
--- src/modules/getFiles/GetFiles.tsx
+++ src/modules/getFiles/GetFiles.tsx
@@ -1,4 +1,7 @@
+import { storage } from "@/config/firebaseConfig";
+import { downloadFileBlob } from "@/db/firebaseStorageSdkUtils";
 import { useFilesStore } from "@/stores/useFilesStore";
+import { $ } from "@/utils/useReactive";
 
 const TableHeadingRowContents = () => {
   return (
@@ -15,6 +18,12 @@ const TableHeadingRowContents = () => {
 
 export const DisplayFilesTable = () => {
   const filesStore = useFilesStore();
+  // const downloadFileBlobStatuses2 = $<
+  //   { id: string; status: "loading" | "success" | "failed"; blob: Blob }[]
+  // >([]);
+  const downloadFileBlobStatuses = $<
+    Record<string, { status: "loading" | "success" | "failed"; blob: Blob | undefined }>
+  >({});
   return (
     <>
       {filesStore.docs.length === 0 && <div>No files</div>}
@@ -36,8 +45,31 @@ export const DisplayFilesTable = () => {
                     <td>{x.createdAt.toDate().toLocaleString()}</td>
                     <td>{x.updatedAt.toDate().toLocaleString()}</td>
                     <td className="text-center">
-                      <button className="btn btn-primary btn-xs" onClick={() => {}}>
-                        \/
+                      <button
+                        className={`btn btn-primary btn-xs`}
+                        onClick={async () => {
+                          downloadFileBlobStatuses.set((statuses) => ({
+                            ...statuses,
+                            [x.id]: { status: "loading", blob: undefined },
+                          }));
+                          const blobResponse = await downloadFileBlob({ storage, id: x.id });
+                          return downloadFileBlobStatuses.set((statuses) => ({
+                            ...statuses,
+                            [x.id]: {
+                              status: blobResponse.success ? "success" : "failed",
+                              blob: blobResponse.data,
+                            },
+                          }));
+                        }}
+                      >
+                        {downloadFileBlobStatuses.value[x.id] === undefined && <>\/</>}
+                        {downloadFileBlobStatuses.value[x.id]?.status === "loading" && (
+                          <span className="loading loading-spinner loading-sm" />
+                        )}
+                        {downloadFileBlobStatuses.value[x.id]?.status === "success" && (
+                          <>Click to download</>
+                        )}
+                        {downloadFileBlobStatuses.value[x.id]?.status === "failed" && <>Failed</>}
                       </button>
                     </td>
                   </tr>
