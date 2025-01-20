8f080e7fd36be5fd0b8fc76e60b19afe65afdbc7
robmolloy <robmolloy@hotmail.co.uk>
2025-01-20 12:47:20 +0000

    improved dx but requires save initializationVector

--- src/modules/getFiles/GetFiles.tsx
+++ src/modules/getFiles/GetFiles.tsx
@@ -1,8 +1,12 @@
 import { storage } from "@/config/firebaseConfig";
 import { downloadFileBlob } from "@/db/firebaseStorageSdkUtils";
+import { fileSchema } from "@/db/firestoreFilesSdk";
 import { useFilesStore } from "@/stores/useFilesStore";
+import { convertBlobToArrayBuffer } from "@/utils/dataTypeUtils";
 import { $ } from "@/utils/useReactive";
 import React from "react";
+import { z } from "zod";
+import { decryptFile, deserializeUInt8Array } from "../encryption";
 
 const TableHeadingRowContents = () => {
   return (
@@ -15,20 +19,118 @@ const TableHeadingRowContents = () => {
     </tr>
   );
 };
+const DisplayFileTableRow = (p: { file: z.infer<typeof fileSchema>; i: number }) => {
+  const $status = $<"init" | "loading" | "encrypted_file_downloaded" | "failed">("init");
+  const $password = $("");
+  const $encryptedBlob = $<Blob>();
+  // const $unencryptedBlob = $<Blob>();
+  return (
+    <>
+      <tr className="b border-b-0">
+        <th className="row-span-2" rowSpan={2}>
+          {p.i + 1}
+        </th>
+        <td>{p.file.name}</td>
+        <td className="max-w-12 overflow-hidden text-ellipsis whitespace-nowrap">
+          {p.file.serializedEncryptionKeySalt}
+        </td>
+        <td>{p.file.createdAt.toDate().toLocaleString()}</td>
+        <td>{p.file.updatedAt.toDate().toLocaleString()}</td>
+      </tr>
+      <tr>
+        <td className="col-span-5 p-2" colSpan={5}>
+          {$status.value}
+          <div className="flex justify-end gap-2">
+            {($status.value === "init" || $status.value === "loading") && (
+              <button
+                className={`btn btn-primary btn-xs`}
+                onClick={async () => {
+                  $status.set("loading");
+
+                  const blobResponse = await downloadFileBlob({ storage, id: p.file.id });
+                  $encryptedBlob.set(blobResponse.success ? blobResponse.data : undefined);
+                  $status.set(blobResponse.success ? "encrypted_file_downloaded" : "failed");
+                }}
+              >
+                {$status.value === "init" && <>Download</>}
+                {$status.value === "loading" && (
+                  <span className="loading loading-spinner loading-sm" />
+                )}
+              </button>
+            )}
+            {$status.value === "failed" && (
+              <button
+                className={`btn btn-error btn-xs`}
+                onClick={async () => {
+                  $status.set("init");
+                }}
+              >
+                Try again
+              </button>
+            )}
+
+            {$encryptedBlob.value && (
+              <>
+                <input
+                  value={$password.value}
+                  onChange={(e) => $password.set(e.target.value)}
+                  type="password"
+                  placeholder="Type here"
+                  className="input input-sm input-bordered w-full max-w-xs"
+                />
+                <button
+                  className={`btn btn-primary btn-xs`}
+                  onClick={async () => {
+                    const saltResponse = deserializeUInt8Array(p.file.serializedEncryptionKeySalt);
+                    if (!$encryptedBlob.value || !saltResponse.success) return;
+                    const initializationVectorResponse = await deserializeUInt8Array(
+                      "[219,88,240,231,244,196,17,133,19,222,121,228]",
+                    );
+                    const encryptedFileBuffer = await convertBlobToArrayBuffer(
+                      $encryptedBlob.value,
+                    );
+                    const initializationVector = initializationVectorResponse.data;
+                    if (!initializationVector) return;
+                    const response = await decryptFile({
+                      initializationVector,
+                      encryptedFileBuffer,
+                      password: $password.value,
+                      salt: saltResponse.data,
+                    });
+                    console.log(`GetFiles.tsx:${/*LL*/ 92}`, { response });
+                  }}
+                >
+                  Decrypt File
+                </button>
+              </>
+            )}
+            {/* {$encryptedBlob.value && (
+              <a
+                className={`btn btn-primary btn-xs`}
+                href={URL.createObjectURL($encryptedBlob.value)}
+                download={p.file.name}
+              >
+                Save Encrypted File
+              </a>
+            )}
+            {$encryptedBlob.value && (
+              <a
+                className={`btn btn-primary btn-xs`}
+                href={URL.createObjectURL($encryptedBlob.value)}
+                download={p.file.name}
+              >
+                <>Save Decrypted File</>
+              </a>
+            )} */}
+          </div>
+        </td>
+      </tr>
+    </>
+  );
+};
 
 export const DisplayFilesTable = () => {
   const filesStore = useFilesStore();
-  const fileStatuses = $<
-    Record<
-      string,
-      {
-        status: "loading" | "encrypted_file_downloaded" | "failed";
-        password?: string;
-        encryptedBlob?: Blob | undefined;
-        unencryptedBlob?: Blob | undefined;
-      }
-    >
-  >({});
 
   if (filesStore.docs.length === 0) return <div>No files</div>;
 
@@ -40,70 +142,9 @@ export const DisplayFilesTable = () => {
         </thead>
         <tbody>
           {filesStore.docs.map((x, j) => {
-            const status = fileStatuses.value[x.id];
             return (
               <React.Fragment key={`display-files-table-${x.id}`}>
-                <tr className="b border-b-0">
-                  <th className="row-span-2" rowSpan={2}>
-                    {j + 1}
-                  </th>
-                  <td>{x.name}</td>
-                  <td className="max-w-12 overflow-hidden text-ellipsis whitespace-nowrap">
-                    {x.serializedEncryptionKeySalt}
-                  </td>
-                  <td>{x.createdAt.toDate().toLocaleString()}</td>
-                  <td>{x.updatedAt.toDate().toLocaleString()}</td>
-                </tr>
-                <tr>
-                  <td className="col-span-5 p-2" colSpan={5}>
-                    {status?.status}
-                    <div className="flex justify-around gap-2">
-                      {status?.status !== "encrypted_file_downloaded" && (
-                        <button
-                          className={`btn btn-primary btn-xs`}
-                          onClick={async () => {
-                            if (status?.status !== undefined) return;
-
-                            const loadingStatus = { status: "loading" } as const;
-                            fileStatuses.set((y) => ({ ...y, [x.id]: loadingStatus }));
-
-                            const blobResponse = await downloadFileBlob({ storage, id: x.id });
-                            const newStatus = {
-                              status: blobResponse.success ? "encrypted_file_downloaded" : "failed",
-                              encryptedBlob: blobResponse.data,
-                            } as const;
-                            return fileStatuses.set((y) => ({ ...y, [x.id]: newStatus }));
-                          }}
-                        >
-                          {status === undefined && <>Download</>}
-                          {status?.status === "loading" && (
-                            <span className="loading loading-spinner loading-sm" />
-                          )}
-                          {status?.status === "failed" && <>Failed</>}
-                        </button>
-                      )}
-
-                      {status?.encryptedBlob && (
-                        <a
-                          className={`btn btn-primary btn-xs`}
-                          href={URL.createObjectURL(status.encryptedBlob)}
-                          download={x.name}
-                        >
-                          <>Save Encrypted File</>
-                        </a>
-                      )}
-                      {status?.encryptedBlob && (
-                        <a
-                          className={`btn btn-primary btn-xs`}
-                          href={URL.createObjectURL(status.encryptedBlob)}
-                          download={x.name}
-                        >
-                          <>Save Decrypted File</>
-                        </a>
-                      )}
-                    </div>
-                  </td>
-                </tr>
+                <DisplayFileTableRow i={j} file={x} />
               </React.Fragment>
             );
           })}
