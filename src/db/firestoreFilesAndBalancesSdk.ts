a2ce8499197a6cf6913b393f4d14127f4bd84ae7
robmolloy <robmolloy@hotmail.co.uk>
2025-01-27 16:26:04 +0000

    success/fail used in createFileAndUpdateBalance

--- src/db/firestoreFilesAndBalancesSdk.ts
+++ src/db/firestoreFilesAndBalancesSdk.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { fileSchema, filesSdk, getNextFileId } from "./firestoreFilesSdk";
 import { balanceSchema, balancesSdk } from "./firestoreBalancesSdk";
 import { creatifyDoc, updatifyDoc } from "@/utils/firestoreSdkUtils/firestoreUtils";
+import { fail, success } from "@/utils/devUtils";
 
 export const createFileAndUpdateBalance = async (p: {
   db: TDb;
@@ -20,22 +21,19 @@ export const createFileAndUpdateBalance = async (p: {
     db: p.db,
     data: creatifyDoc({ id: fileId, uid: p.balance.uid, ...p.file }),
   });
-  if (!createFileResponse.success) return createFileResponse;
+  if (!createFileResponse.success) return fail({ error: { message: "create file failed" } });
 
   const getFileResponse = await filesSdk.getDoc({ db: p.db, id: fileId });
-  if (!getFileResponse.success) return getFileResponse;
+  if (!getFileResponse.success) return fail({ error: { message: "get file failed" } });
 
   const updateBalanceResponse = await balancesSdk.updateDoc({
     db: p.db,
     data: updatifyDoc({ ...p.balance, numberOfCoupons: p.balance.numberOfCoupons - 1 }),
   });
-  if (!updateBalanceResponse.success) return updateBalanceResponse;
+  if (!updateBalanceResponse.success) return fail({ error: { message: "update balance failed" } });
 
   const getBalanceResponse = await balancesSdk.getDoc({ db: p.db, id: p.balance.uid });
-  if (!getBalanceResponse.success) return getBalanceResponse;
+  if (!getBalanceResponse.success) return fail({ error: { message: "getUpdatedBalance failed" } });
 
-  return {
-    success: true,
-    data: { file: getFileResponse.data, balance: getBalanceResponse.data },
-  } as const;
+  return success({ data: { file: getFileResponse.data, balance: getBalanceResponse.data } });
 };
